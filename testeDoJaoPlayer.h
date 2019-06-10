44da667fd38423de992c56638a0d2466e569bd92
JaoSchmidt <51456769+JaoSchmidt@users.noreply.github.com>
2019-06-10 15:12:11 -0300

    Update testeDoJaoPlayer.h

--- testeDoJaoPlayer.h
+++ testeDoJaoPlayer.h
@@ -5,15 +5,14 @@ typedef struct infoJogador{
     WINDOW *curWin;
     int locX,locY,maxY,maxX;
 }jogador;
-
+int k = 10;
 jogador initialize_player(WINDOW *win,int y,int x,jogador p1){
     p1.locY = y;
     p1.locX = x;
-    p1.curWin = win;
+    p1.curWin = win;//current window
     getmaxyx(p1.curWin,p1.maxY,p1.maxX);
     return p1;
 }
-/*
 void movUp(jogador *pp1){
     if(pp1->locY>0){
         pp1->locY--;
@@ -32,7 +31,12 @@ void movRight(jogador *pp1){
 void movDown(jogador *pp1){
     if(pp1->locY<pp1->maxY){
         pp1->locY++;
+        k++;
+        mvwprintw(stdscr,2,2,"%d",pp1->locY);
+        mvwprintw(stdscr,3,2,"%d",k);
+        wrefresh(stdscr);
     }
+    
 }
 int Display(jogador p1){
     int choice = wgetch(p1.curWin);
@@ -40,23 +44,30 @@ int Display(jogador p1){
     {
     case KEY_UP:
         movUp(&p1);
+        mvwprintw(stdscr,1,2,"up");
+        wrefresh(stdscr);
         break;
     case KEY_DOWN:
         movDown(&p1);
+        mvwprintw(stdscr,1,2,"down");
+        wrefresh(stdscr);
         break;
     case KEY_LEFT:
         movLeft(&p1);
+        mvwprintw(stdscr,1,2,"left");
+        wrefresh(stdscr);
         break;
     case KEY_RIGHT:
         movRight(&p1);
+        mvwprintw(stdscr,1,2,"right");
+        wrefresh(stdscr);
         break;
     default:
         break;
     }
     return choice;
 }
-*/
-jogador movPlayer(jogador p1){
+
+void movPlayer(jogador p1){
     mvwprintw(p1.curWin,p1.locY,p1.locX,"aa");
-    return p1;
 }
