6ddda4dda24ea6c50ac5ec98c82e09fd8d8bbe2d
JaoSchmidt <51456769+JaoSchmidt@users.noreply.github.com>
2019-06-21 03:27:23 -0300

    Update enemy.h

--- enemy.h
+++ enemy.h
@@ -11,8 +11,8 @@ inimigo *inicializainimigo(int cont,int altura,int largura){
     inimigo *i = (inimigo*) malloc(sizeof(inimigo));
     i->tempo=cont;
     i->vivo=1; //cada um nasce com o tempo diferente
-    i->x=distanciainimigos*cont+40;
-    i->y=distanciainimigos*cont;
+    i->x=DISTANCIA_INIMIGOS*cont+60;
+    i->y=DISTANCIA_INIMIGOS*cont;
     return i;
 }
 
@@ -33,44 +33,26 @@ void controleinimigo(inimigo *i,jogador *p){
                 i->y++;
             }
         }
+        mvwprintw(p->curWin,i->y,i->x,"\\o/");
     }
 }
 
-void desenhainimigo(inimigo *i,jogador *p){
-    if(i->vivo==1)
-        mvwprintw(p->curWin,i->y,i->x,"\\o/");
-}
-
-void colisaotiro(jogador *p,inimigo *i,int *score){ 
+void colisaotiro(tiro *t[],inimigo *i,int *score,jogador *p,int maxY,int maxX){ 
     if(i->vivo==1){
         int cont;
-        mvwprintw(p->curWin,1,1,"aaaaaaaa");
-        for(cont=0;cont<p->qtiroscima;cont++) //aqui encontrei um problema pois era necessario retornar dois valores: p e i
-            if(p->tirocimax[cont]==i->x+1)
-                if(p->tirocimay[cont]==i->y||p->tirocimay[cont]==i->y-1||p->tirocimay[cont]==i->y+1){
-                    i->vivo=0;*(p->tirocimax+cont)=1000;(*score)++;
-                    mvwprintw(p->curWin,i->y,i->x,"   ");
+        for(cont=0;cont<p->qtiros;cont++){
+            if(t[cont]->vivo==1){
+                if(t[cont]->locx==i->x+1 && t[cont]->locy==i->y){
+                    t[cont]->vivo=0;
+                    i->vivo=0;
+                    mvwprintw(t[cont]->curWin,i->y,i->x,"   ");
                 }
-        for(cont=0;cont<p->qtirosbaixo;cont++)
-            if(p->tirobaixox[cont]==i->x+1)
-                if(p->tirobaixoy[cont]==i->y||p->tirobaixoy[cont]==i->y-1||p->tirobaixoy[cont]==i->y+1){
-                    i->vivo=0;*(p->tirobaixox+cont)=1000;(*score)++;
-                    mvwprintw(p->curWin,i->y,i->x,"   ");
+                if(t[cont]->locx<0||t[cont]->locx>maxX||t[cont]->locy<0||t[cont]->locy>maxY){
+                    t[cont]->vivo=0;
+                    mvwaddch(t[cont]->curWin,t[cont]->locy,t[cont]->locx,' ');
                 }
-        for(cont=0;cont<p->qtirosesquerda;cont++)
-            if(p->tiroesquerday[cont]==i->y)
-                if(p->tiroesquerdax[cont]==i->x+1||p->tiroesquerdax[cont]==i->x||p->tiroesquerdax[cont]==i->x-1){
-                    i->vivo=0;*(p->tiroesquerday+cont)=1000;(*score)++;
-                    mvwprintw(p->curWin,i->y,i->x,"   ");
-                }
-        
-        for(cont=0;cont<p->qtirosdireita;cont++)
-            if(p->tirodireitay[cont]==i->y)
-                if(p->tirodireitax[cont]==i->x+1||p->tirodireitax[cont]==i->x||p->tirodireitax[cont]==i->x-1){
-                    i->vivo=0;*(p->tirodireitay+cont)=1000;(*score)++;
-                    mvwprintw(p->curWin,i->y,i->x,"   "); 
-                    
-                }
+            }
+        }
     }
 }
 
