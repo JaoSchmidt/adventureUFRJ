8a026326da77215c36ba317c9f248b5d25b85b40
JaoSchmidt <51456769+JaoSchmidt@users.noreply.github.com>
2019-06-22 02:16:29 -0300

    Update enemy.h

--- enemy.h
+++ enemy.h
@@ -7,27 +7,23 @@ typedef struct _INIMIGO{ //estrutura para inimigo
     unsigned short int vivo; //essa flag indica se o bicho está vivo (bool)
 } inimigo;
 
-inimigo *inicializa_inimigo(int cont,int altura,int largura){
+inimigo *inicializa_inimigo(int vivo,int altura,int largura){
     inimigo *i = (inimigo*) malloc(sizeof(inimigo));
-    i->tempo=cont;
-    i->vivo=1; //cada um nasce com o tempo diferente
-    i->x=DISTANCIA_INIMIGOS*cont+60;
-    i->y=DISTANCIA_INIMIGOS*cont;
+    i->vivo=vivo; 
+    i->x=largura;
+    i->y=altura;
     return i;
 }
 
-void controleinimigo(inimigo *i,jogador *p){
+void controle_inimigo0(inimigo *i,jogador *p){
     if(i->vivo==1){
         mvwprintw(p->curWin,i->y,i->x,"   ");
         float tg = ((float)(i->y-p->locY))/(i->x-p->locX);
         float cotg = 1/tg;
-        mvwprintw(p->curWin,7,2,"%f",tg);
         if(i->x-p->locX>=0 && tg>=-1 && tg<=1){
-            mvwprintw(p->curWin,5,2,"1f = %d %f",i->x-p->locX,tg);
             i->y += tg*(-2);
             i->x-=2;
         }else if(i->x-p->locX<=0 && tg>=-1 && tg<=1){
-            mvwprintw(p->curWin,5,2,"2f = %d %f",i->x-p->locX,tg);
             i->y += tg*2;
             i->x+=2;
         }else if(i->y-p->locY>0 && cotg<1 && cotg>-1){
@@ -41,23 +37,154 @@ void controleinimigo(inimigo *i,jogador *p){
     }
 }
 
-void colisaotiro(tiro *t[],inimigo *i,int *score,jogador *p,int maxY,int maxX){ 
+void controle_inimigo_1(inimigo *i,jogador *p){
+    if(i->vivo==1){
+        mvwaddch(p->curWin,i->y-1,i->x,' ');
+        mvwaddch(p->curWin,i->y,i->x,' ');
+        mvwaddch(p->curWin,i->y+1,i->x,' ');
+        if(i->x>p->locX){
+            i->x-=2;
+        }
+        if(i->x<p->locX){
+            i->x+=2;
+        }
+        if(i->x==p->locX||i->x==p->locX-1||i->x==p->locX+1){
+            if(i->y>p->locY){
+                i->y--;
+            }
+            if(i->y<p->locY){
+                i->y++;
+            }
+        }
+        mvwaddch(p->curWin,i->y-1,i->x,'^');
+        mvwaddch(p->curWin,i->y,i->x,'O');
+        mvwaddch(p->curWin,i->y+1,i->x,'v');
+    }
+}
+
+void controle_inimigo_2(inimigo *i,jogador *p){
+    if(i->vivo==1){
+        mvwprintw(p->curWin,i->y,i->x,"   ");
+        if(i->y>p->locY){
+            i->y--;
+        }
+        if(i->y<p->locY){
+            i->y++;
+        }
+        if(i->y==p->locY){
+            if(i->x>p->locX){
+                i->x-=2;
+            }
+            if(i->x<p->locX){
+                i->x+=2;
+            }
+        }
+        mvwprintw(p->curWin,i->y,i->x,"<O>");
+    }
+}
+
+
+void colisao_tiro_inimigo_0e1(tiro *t[],inimigo *i,int *score,jogador *p,int maxY,int maxX,int *enemy_number,WINDOW *stats){ 
     if(i->vivo==1){
         int cont;
-        for(cont=0;cont<MAX_SHOOT;cont++){
+        for(cont=0;cont<MAX_SHOTS;cont++){
             if(t[cont]->vivo==1){
                 if(t[cont]->locx==i->x+1 && t[cont]->locy==i->y){
                     t[cont]->vivo=0;
                     i->vivo=0;
                     mvwprintw(t[cont]->curWin,i->y,i->x,"   ");
-                }
-                if(t[cont]->locx<0||t[cont]->locx>maxX||t[cont]->locy<0||t[cont]->locy>maxY){
-                    t[cont]->vivo=0;
-                    mvwaddch(t[cont]->curWin,t[cont]->locy,t[cont]->locx,' ');
+                    //STATS MODIFICATION
+                    *score+=3;
+                    if(*score<0){
+                        *score = -*score;
+                        mvwprintw(stats,getmaxy(stats)/2-1,8,"-%d%d%d%d",((*score)/1000)%10,((*score)/100)%10,((*score)/10)%10,(*score)%10);
+                        *score = -*score;
+                    }else{
+                        mvwprintw(stats,getmaxy(stats)/2-1,8," %d%d%d%d",((*score)/1000)%10,((*score)/100)%10,((*score)/10)%10,(*score)%10);
+                    }
+                    wrefresh(stats);
+                    *enemy_number-=1;
                 }
             }
         }
     }
 }
-
+void colisao_tiro_inimigo_2(tiro *t[],inimigo *i,int *score,jogador *p,int maxY,int maxX,int *enemy_number,WINDOW*stats){ 
+    if(i->vivo==1){
+        int cont;
+        for(cont=0;cont<MAX_SHOTS;cont++){
+            if(t[cont]->vivo==1){
+                if(t[cont]->locx==i->x && t[cont]->locy==i->y){
+                    mvwaddch(t[cont]->curWin,i->y,i->x,' ');
+                    mvwaddch(t[cont]->curWin,i->y-1,i->x,' ');
+                    mvwaddch(t[cont]->curWin,i->y+1,i->x,' ');
+                    t[cont]->vivo=0;
+                    i->vivo=0;
+                    //STATS MODIFICATION
+                    *score+=3;
+                    if(*score<0){
+                        *score = -*score;
+                        mvwprintw(stats,getmaxy(stats)/2-1,8,"-%d%d%d%d",((*score)/1000)%10,((*score)/100)%10,((*score)/10)%10,(*score)%10);
+                        *score = -*score;
+                    }else{
+                        mvwprintw(stats,getmaxy(stats)/2-1,8," %d%d%d%d",((*score)/1000)%10,((*score)/100)%10,((*score)/10)%10,(*score)%10);
+                    }
+                    wrefresh(stats);
+                    *enemy_number-=1;
+                }
+            }
+        }
+    }
+}
+void colisao_player_inimigos_0e1(jogador *pcolisao,inimigo *icolisao,int *score,int *enemy_number,WINDOW *stats){
+    if(icolisao->vivo==1){
+        if((icolisao->x+1==pcolisao->locX+1)&&icolisao->y == pcolisao->locY){
+            icolisao->vivo=0;
+            int maxy = getmaxy(stats);
+            mvwprintw(pcolisao->curWin,icolisao->y,icolisao->x,"   ");
+            pcolisao->vida--;
+            //STATS MODIFICATION
+            *score-=10;
+            if(*score<0){
+                *score = -*score;
+                mvwprintw(stats,maxy/2-1,8,"-%d%d%d%d",((*score)/1000)%10,((*score)/100)%10,((*score)/10)%10,(*score)%10);
+                *score = -*score;
+            }else{
+                mvwprintw(stats,maxy/2-1,8," %d%d%d%d",((*score)/1000)%10,((*score)/100)%10,((*score)/10)%10,(*score)%10);
+            }
+            mvwprintw(stats,maxy/2+1,8+pcolisao->vida," ");
+            if(pcolisao->vida == 9)
+                mvwprintw(stats,maxy/2+1,21," ");
+            mvwprintw(stats,maxy/2+1,22,"%d",pcolisao->vida);
+            wrefresh(stats);
+            *enemy_number-=1;
+        }
+    }
+}
+void colisao_player_inimigos2(jogador *pcolisao,inimigo *icolisao,int *score,int *enemy_number,WINDOW *stats){
+    if(icolisao->vivo==1){
+        if(icolisao->x==pcolisao->locX+1&&icolisao->y == pcolisao->locY){
+            icolisao->vivo=0;
+            int maxy = getmaxy(stats);
+            mvwprintw(pcolisao->curWin,icolisao->y,icolisao->x,"   ");
+            mvwaddch(pcolisao->curWin,icolisao->y-1,icolisao->x,' ');
+            mvwaddch(pcolisao->curWin,icolisao->y+1,icolisao->x,' ');
+            *score-=10;
+            if(*score<0){
+                *score = -*score;
+                mvwprintw(stats,maxy/2-1,8,"-%d%d%d%d",((*score)/1000)%10,((*score)/100)%10,((*score)/10)%10,(*score)%10);
+                *score = -*score;
+            }else{
+                mvwprintw(stats,maxy/2-1,8," %d%d%d%d",((*score)/1000)%10,((*score)/100)%10,((*score)/10)%10,(*score)%10);
+            }
+            pcolisao->vida--;
+            mvwprintw(stats,maxy/2+1,8+pcolisao->vida," ");
+            if(pcolisao->vida == 9)
+                mvwprintw(stats,maxy/2+1,21," ");
+            mvwprintw(stats,maxy/2+1,22,"%d",pcolisao->vida);
+            wrefresh(stats);
+            *enemy_number-=1;
+        }
+    }
+}
 #endif
