9102f078e96bf041ab8b4da982426487921816e9
JaoSchmidt <51456769+JaoSchmidt@users.noreply.github.com>
2019-06-25 15:47:48 -0300

    Update time_elapsed.h

--- time_elapsed.h
+++ time_elapsed.h
@@ -3,7 +3,10 @@
 #define TIME_ELAPSED_HEADER
 
 struct timespec spec;
-
+/*This function return false if elapsed time since it was 
+called is smaller than the value of the second parameter 
+in seconds. If value is bigger, returns true and reset 
+the time elapsed*/
 float time_elapsed(float *tv0,float limit){
     clock_gettime(CLOCK_MONOTONIC, &spec);
     float a;
@@ -16,3 +19,12 @@ float time_elapsed(float *tv0,float limit){
 }
 
 #endif
+
+/*CLOCK_MONOTONIC represents the absolute elapsed wall-clock 
+time since some arbitrary, fixed point in the past. It isn't
+ affected by changes in the system time-of-day clock.
+
+If you want to compute the elapsed time between two events 
+observed on the one machine without an intervening reboot, 
+CLOCK_MONOTONIC is the best option.*/
+
