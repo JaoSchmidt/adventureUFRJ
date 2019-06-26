f80842cfa10bda4478e147f5c5d2e92c4db9fefb
JaoSchmidt <51456769+JaoSchmidt@users.noreply.github.com>
2019-06-26 18:39:02 -0300

    Update enemy.h

--- enemy.h
+++ enemy.h
@@ -1,20 +1,20 @@
 #ifndef ENEMY_HEADER
 #define PLAYER_HEADER
-
+ 
 typedef struct _INIMIGO{ //estrutura para inimigo
     int x,y; //coordenadas do inimigo
     unsigned short int tempo; //essa flag vai controlar a velocidade do inimigo (indiretamente)
     unsigned short int vivo; //essa flag indica se o bicho está vivo (bool)
 } inimigo;
-
+ 
 inimigo *inicializa_inimigo(int vivo,int altura,int largura){
     inimigo *i = (inimigo*) malloc(sizeof(inimigo));
-    i->vivo=vivo; 
+    i->vivo=vivo;
     i->x=largura;
     i->y=altura;
     return i;
 }
-
+ 
 ////ENEMY MOVEMENT AND DRAW
 void controle_inimigo0(inimigo *i,jogador *p){
     if(i->vivo==1){
@@ -37,7 +37,7 @@ void controle_inimigo0(inimigo *i,jogador *p){
         mvwprintw(p->curWin,i->y,i->x,"OoO");
     }
 }
-
+ 
 void controle_inimigo_2(inimigo *i,jogador *p){
     if(i->vivo==1){
         mvwaddch(p->curWin,i->y-1,i->x,' ');
@@ -62,7 +62,7 @@ void controle_inimigo_2(inimigo *i,jogador *p){
         mvwaddch(p->curWin,i->y+1,i->x,'v');
     }
 }
-
+ 
 void controle_inimigo_1(inimigo *i,jogador *p){
     if(i->vivo==1){
         mvwprintw(p->curWin,i->y,i->x,"   ");
@@ -84,7 +84,7 @@ void controle_inimigo_1(inimigo *i,jogador *p){
     }
 }
 ////COLISION ENEMY WITH SHOTS
-void colisao_tiro_inimigo_0e1(tiro *t[],inimigo *i,int *score,int maxY,int maxX,int *enemy_number,WINDOW *stats){ 
+void colisao_tiro_inimigo_0e1(tiro *t[],inimigo *i,int *score,int maxY,int maxX,int *enemy_number,WINDOW *stats){
     if(i->vivo==1){
         int cont;
         for(cont=0;cont<MAX_SHOTS;cont++){
@@ -104,16 +104,16 @@ void colisao_tiro_inimigo_0e1(tiro *t[],inimigo *i,int *score,int maxY,int maxX,
                     }else{
                         mvwprintw(stats,getmaxy(stats)/2-1,8," %d%d%d%d",((*score)/1000)%10,((*score)/100)%10,((*score)/10)%10,(*score)%10);
                     }
-                    wrefresh(stats);
                     *enemy_number-=1;
                     mvwprintw(stats,maxy/2+1,maxx/2,"Enemies alive: %d%d",((*enemy_number)/10)%10,(*enemy_number)%10);
+                    wrefresh(stats);
                 }
             }
         }
     }
 }
-
-void colisao_tiro_inimigo_2(tiro *t[],inimigo *i,int *score,int maxY,int maxX,int *enemy_number,WINDOW*stats){ 
+ 
+void colisao_tiro_inimigo_2(tiro *t[],inimigo *i,int *score,int maxY,int maxX,int *enemy_number,WINDOW*stats){
     if(i->vivo==1){
         int cont;
         for(cont=0;cont<MAX_SHOTS;cont++){
@@ -135,9 +135,9 @@ void colisao_tiro_inimigo_2(tiro *t[],inimigo *i,int *score,int maxY,int maxX,in
                     }else{
                         mvwprintw(stats,getmaxy(stats)/2-1,8," %d%d%d%d",((*score)/1000)%10,((*score)/100)%10,((*score)/10)%10,(*score)%10);
                     }
-                    wrefresh(stats);
                     *enemy_number-=1;
                     mvwprintw(stats,maxy/2+1,maxx/2,"Enemies alive: %d%d",((*enemy_number)/10)%10,(*enemy_number)%10);
+                    wrefresh(stats);
                 }
             }
         }
@@ -162,14 +162,12 @@ void colisao_player_inimigos_0e1(jogador *pcolisao,inimigo *icolisao,int *score,
                 mvwprintw(stats,maxy/2-1,8," %d%d%d%d",((*score)/1000)%10,((*score)/100)%10,((*score)/10)%10,(*score)%10);
             }
             mvwprintw(stats,maxy/2+1,8+pcolisao->vida," ");
-            if(pcolisao->vida == 9)
-                mvwprintw(stats,maxy/2+1,21," ");
             if(pcolisao->vida == 0)
                 *pc=112;
             mvwprintw(stats,maxy/2+1,22,"%d",pcolisao->vida);
-            wrefresh(stats);
             *enemy_number-=1;
             mvwprintw(stats,maxy/2+1,maxx/2,"Enemies alive: %d%d",((*enemy_number)/10)%10,(*enemy_number)%10);
+            wrefresh(stats);
         }
     }
 }
@@ -193,14 +191,12 @@ void colisao_player_inimigos2(jogador *pcolisao,inimigo *icolisao,int *score,int
                 mvwprintw(stats,maxy/2-1,8," %d%d%d%d",((*score)/1000)%10,((*score)/100)%10,((*score)/10)%10,(*score)%10);
             }
             mvwprintw(stats,maxy/2+1,8+pcolisao->vida," ");
-            if(pcolisao->vida == 9)
-                mvwprintw(stats,maxy/2+1,21," ");
             if(pcolisao->vida == 0)
                 *pc=112;
             mvwprintw(stats,maxy/2+1,22,"%d",pcolisao->vida);
-            wrefresh(stats);
             *enemy_number-=1;
             mvwprintw(stats,maxy/2+1,maxx/2,"Enemies alive: %d%d",((*enemy_number)/10)%10,(*enemy_number)%10);
+            wrefresh(stats);
         }
     }
 }
